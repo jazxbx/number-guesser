12b7d0d71b17f4169d7cc44f79575e6b09221034
jazz <jazxbx@gmail.com>
2023-08-11 20:14:01 -0700

    modified regular game

--- index.js
+++ index.js
@@ -2,17 +2,11 @@
 
 "use strict";
 
-const secretNum = Math.ceil(Math.random() * 10);
-let attempts = 5;
-let tries = 0;
+const AVAILABLE_ATTEMPTS = 5;
 let isActive = true;
 let guess = 0;
 let minNumber = 1;
 let maxNumber = 5000;
-
-console.log(`---guess --${guess}`);
-
-console.log(secretNum, attempts);
 
 // randomRNum generator
 function getRandomRange(minNumber, maxNumber) {
@@ -60,30 +54,40 @@ function validateNumbers(minNumber, maxNumber) {
 // Function to handle regular game mode
 
 const playRegularGame = () => {
-  while (attempts > 0) {
+  let secretNum = Math.ceil(Math.random() * 10); //moved to local scope to prevent unpredictable side effects
+  let tries = 0;
+  while (tries < AVAILABLE_ATTEMPTS) {
     guess = +prompt(
-      `Guess a number from 1-10. You have ${attempts} attempts remaining.`
+      `Guess a number from 1-10. You have ${
+        AVAILABLE_ATTEMPTS - tries
+      } attempts remaining.`
     );
 
-    if (isNaN(guess) || guess < 1 || guess > 10) {
+    if (isNaN(guess) || guess < 1 || guess > 10 || !guess) {
       alert("Please enter a valid number between 1 and 10.");
       continue;
     }
 
+    tries++;
     if (guess === secretNum) {
       alert(
-        `🥳 You guessed correctly! The number was ${secretNum}. You guessed it in ${attempts} tries.`
+        `🥳 You guessed correctly! The number was ${secretNum}. You guessed it in ${tries} tries.`
       );
-      attempts--;
       break;
     } else if (guess > secretNum) {
-      alert(`Number is too high ☝️ Guess again.`);
+      alert(
+        `Number is too high ☝️ ${
+          tries != AVAILABLE_ATTEMPTS ? "Guess again!" : ""
+        }`
+      );
     } else {
-      alert(`Number is too low 👇Guess again.`);
+      alert(
+        `Number is too low 👇 ${
+          tries != AVAILABLE_ATTEMPTS ? "Guess again!" : ""
+        }`
+      );
     }
-    attempts--;
-
-    if (attempts === 0) {
+    if (tries === AVAILABLE_ATTEMPTS) {
       alert(
         `Sorry, you have run out of attempts. The correct number was ${secretNum}.`
       );
@@ -93,60 +97,60 @@ const playRegularGame = () => {
 
 //Function to handle bonus game mode
 
-const playBonusGame = () => {
-  alert(
-    `You are playing bonus mode!🥳 You get to choose the range of numbers to guess!👌`
-  );
+// const playBonusGame = () => {
+//   alert(
+//     `You are playing bonus mode!🥳 You get to choose the range of numbers to guess!👌`
+//   );
 
-  validateNumbers(minNumber, maxNumber);
-  // const minNumber = +prompt(`Input the minimum number`);
-  // const maxNumber = +prompt(`Input the maximum number`);
-  // console.log(`---minimum-${minNumber}, maximum-${maxNumber}`);
-  const randomNum = getRandomRange(minNumber, maxNumber);
-  // console.log(`---random range ${randomNum}`);
-  guess = +prompt(`Guess the random number!`);
+//   validateNumbers(minNumber, maxNumber);
+//   // const minNumber = +prompt(`Input the minimum number`);
+//   // const maxNumber = +prompt(`Input the maximum number`);
+//   // console.log(`---minimum-${minNumber}, maximum-${maxNumber}`);
+//   const randomNum = getRandomRange(minNumber, maxNumber);
+//   // console.log(`---random range ${randomNum}`);
+//   guess = +prompt(`Guess the random number!`);
 
-  let bonusAttempts = true;
+//   let bonusAttempts = true;
 
-  // while (bonusAttempts=true) incorrect syntax
-  while (bonusAttempts) {
-    if (isNaN(guess) || guess < minNumber) {
-      alert(`No number! 😖`);
-      guess = +prompt(`Guess the random number! You have ${attempts} left`);
-      continue;
-    }
+//   // while (bonusAttempts=true) incorrect syntax
+//   while (bonusAttempts) {
+//     if (isNaN(guess) || guess < minNumber) {
+//       alert(`No number! 😖`);
+//       guess = +prompt(`Guess the random number! You have ${attempts} left`);
+//       continue;
+//     }
 
-    if (guess === null) {
-      alert(`Goodbye!`);
-      break;
-    }
+//     if (guess === null) {
+//       alert(`Goodbye!`);
+//       break;
+//     }
 
-    if (guess === randomNum) {
-      if (attempts > 1) {
-        tries++;
-        alert(`You win! You guessed correctly in ${tries} tries.`);
-      }
-      bonusAttempts = false;
-    } else if (guess > randomNum) {
-      if (attempts > 1) {
-        alert(`Guess is too high ☝️`);
-        attempts--;
-        guess = +prompt(`Guess the random number! You have ${attempts} left`);
-      } else {
-        alert(`You lost the game! The number was ${secretNum}`);
-      }
-    } else if (guess < randomNum) {
-      if (attempts > 1) {
-        alert(`Guess too low 👇`);
-        attempts--;
-        guess = +prompt(`Guess the random number! You have ${attempts} left`);
-      } else {
-        alert(`You lost the game! The random number was ${secretNum}!`);
-        bonusAttempts = false;
-      }
-    }
-  }
-};
+//     if (guess === randomNum) {
+//       if (attempts > 1) {
+//         tries++;
+//         alert(`You win! You guessed correctly in ${tries} tries.`);
+//       }
+//       bonusAttempts = false;
+//     } else if (guess > randomNum) {
+//       if (attempts > 1) {
+//         alert(`Guess is too high ☝️`);
+//         attempts--;
+//         guess = +prompt(`Guess the random number! You have ${attempts} left`);
+//       } else {
+//         alert(`You lost the game! The number was ${secretNum}`);
+//       }
+//     } else if (guess < randomNum) {
+//       if (attempts > 1) {
+//         alert(`Guess too low 👇`);
+//         attempts--;
+//         guess = +prompt(`Guess the random number! You have ${attempts} left`);
+//       } else {
+//         alert(`You lost the game! The random number was ${secretNum}!`);
+//         bonusAttempts = false;
+//       }
+//     }
+//   }
+// };
 
 // Function to determine game mode
 function chooseGamemode() {
@@ -154,6 +158,11 @@ function chooseGamemode() {
     let gameMode = prompt(
       `Welcome to Number Guesser! Do you want to play on regular or bonus mode?`
     );
+
+    if (gameMode === null) {
+      alert(`Goodbye!`);
+      break;
+    }
     gameMode = gameMode.trim(); // DO NOT FORGET ASSIGNMENT OPERATOR. Reassigned trim value of gameMode
     if (
       gameMode === "regular" ||
@@ -169,9 +178,6 @@ function chooseGamemode() {
       // gameMode === "bonus mode "
     ) {
       playBonusGame();
-    } else if (gameMode === null) {
-      alert(`Goodbye!`);
-      break;
     } else {
       alert(`Not a valid response`);
       isActive = false;
